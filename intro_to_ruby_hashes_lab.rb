073c200b4456f7c863c8179bfdd93433716bfeba
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 14:25:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:rent_in_dollars][:two_piece_owned] =50
 	monopoly[:railroads][:rent_in_dollars][:three_piece_owned] =100
 	monopoly[:railroads][:rent_in_dollars][:four_piece_owned] =200
-	monopoly[:railroads][:names][] ={}
+	monopoly[:railroads][:names][:reading_railroad] ={}
 
 	return monopoly
 end
