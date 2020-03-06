a3498401b7e1f26131b75bf392281d2b6be1741c
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 14:20:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,6 +18,7 @@ def monopoly_with_second_tier
 	monopoly = {}
 	monopoly[:railroads] = {}
 	monopoly[:railroads][:pieces] = 4
+	monopoly[:railroads][:name] ={}
 	monopoly[:railroads][:rent_in_dollars] ={}
 	return monopoly
 end
